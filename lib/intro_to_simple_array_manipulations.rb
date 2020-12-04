80b47d0227a096628563ffafca663bcc2a618e5a
Chip Lempke <clempke90@gmail.com>
2020-12-04 22:41:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array, element)
-  array.insert(element)
+def using_insert(a, b)
+  a.insert(b)
 end
 
 def using_uniq(array)
